5d143db4fe612c18ec20c18e00afa8ebdfc9406c
shin-SeungHun <sng7387@gmail.com>
2024-01-12 01:56:28 +0900

    feat: go_router to editScreen

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -2,6 +2,8 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import '../ui/add/add_screen.dart';
 import '../ui/add/add_view_model.dart';
+import '../ui/edit/edit_screen.dart';
+import '../ui/edit/edit_view_model.dart';
 import '../ui/main/main_screen.dart';
 import '../ui/main/main_view_model.dart';
 
@@ -19,10 +21,18 @@ final router = GoRouter(initialLocation: '/', routes: [
       path: '/detailScreen',
       name: 'DetailScreen',
       builder: (context, state) {
-        // final NoteItem? noteItems = state['note.db'] as NoteItem?;
         return ChangeNotifierProvider.value(
           value: AddViewModel(),
-          child:   const AddScreen(),
+          child: const AddScreen(),
+        );
+      }),
+  GoRoute(
+      path: '/editScreen',
+      name: 'EditScreen',
+      builder: (context, state) {
+        return ChangeNotifierProvider.value(
+          value: EditViewModel(),
+          child: const EditScreen(),
         );
       }),
 ]);
