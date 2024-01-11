b58a9ca0312518b5538169e821f047af5f58de8f
shin-SeungHun <sng7387@gmail.com>
2024-01-11 16:41:13 +0900

    refactoring: add_note_screen

--- lib/ui/add_note/add_note_screen.dart
+++ lib/ui/add_note/add_note_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../colors.dart';
+
 class AddNoteScreen extends StatefulWidget {
   const AddNoteScreen({super.key});
 
@@ -11,6 +13,14 @@ class AddNoteScreen extends StatefulWidget {
 class _AddNoteScreenState extends State<AddNoteScreen> {
   final _titleController = TextEditingController();
   final _contentController = TextEditingController();
+
+  final List<Color> noteColors = [
+    roseBud,
+    primrose,
+    wisteria,
+    skyBlue,
+    illusion,
+  ];
 
   @override
   void initState() {
@@ -33,7 +43,79 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
       ),
       body: AnimatedContainer(
         duration: const Duration(milliseconds: 500),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 16, right: 16, bottom: 16, top: 20),
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: noteColors
+                    .map(
+                      (e) => InkWell(
+                        onTap: () {},
+                        child: _buildBackgroundColor(color: e, selected: false),
+                      ),
+                    )
+                    .toList(),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: _titleController,
+                maxLines: 1,
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(color: darkGray),
+                decoration: const InputDecoration(
+                  hintText: '제목을 입력하세요',
+                  border: InputBorder.none,
+                ),
+              ),
+              TextField(
+                controller: _contentController,
+                maxLines: null,
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: darkGray,
+                    ),
+                decoration: const InputDecoration(
+                  hintText: '내용을 입력하세요',
+                  border: InputBorder.none,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+Widget _buildBackgroundColor({
+  required Color color,
+  required bool selected,
+}) {
+  return Container(
+    width: 48,
+    height: 48,
+    decoration: BoxDecoration(
+      color: color,
+      shape: BoxShape.circle,
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.2),
+          blurRadius: 5.0,
+          spreadRadius: 1.0,
+        )
+      ],
+      border: selected
+          ? Border.all(
+              color: Colors.black,
+              width: 3.0,
+            )
+          : null,
+    ),
+  );
 }
