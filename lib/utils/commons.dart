e0519cffaf258f23a45dc71bc048ef8bf0177606
shin-SeungHun <sng7387@gmail.com>
2024-01-12 18:03:25 +0900

    feat: timeStamp formatting method

--- lib/utils/commons.dart
+++ lib/utils/commons.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_note_application/utils/enum/custom_colors.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 /// 문자열(숫자)를 입력 받아,[ enum CustomColors ] 값의 컬러 값 을 리턴 한다.
 /// 예) color: Commons.convertIndexToColor(index: noteItems.getAt(index)!.color.toString()),
@@ -26,4 +28,12 @@ class Commons {
     }
   }
 
+  /// 숫자를 입력 받아, String 값의 날짜를 리턴한다.
+  /// 예) Text(Commons.convertTime(timeStamp:  noteItems.getAt(index)!.timeStamp,),)
+  static String convertTime({required int timeStamp}) {
+    initializeDateFormatting('ko_KR', null);
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(timeStamp);
+    DateFormat formatter = DateFormat('yyyy년 M월 d일 EEEE H시 m분', 'ko_KR');
+    return formatter.format(date);
+  }
 }
