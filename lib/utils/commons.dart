b34ec611198b2bbe0fd1756b8ba6cfe564c1ada8
Hyojun Cho <lionjerher84@gmail.com>
2024-01-12 16:59:32 +0900

    feat: 리스트뷰 위젯 파일 추가 -> 메인 스크린에서  분할, 상대 -> 절대 경로 변경

--- lib/utils/commons.dart
+++ lib/utils/commons.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'enum/custom_colors.dart';
+import 'package:flutter_note_application/utils/enum/custom_colors.dart';
 
 /// 문자열(숫자)를 입력 받아,[ enum CustomColors ] 값의 컬러 값 을 리턴 한다.
 /// 예) color: Commons.convertIndexToColor(index: noteItems.getAt(index)!.color.toString()),
