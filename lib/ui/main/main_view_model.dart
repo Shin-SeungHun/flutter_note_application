93cbef969676a9963161d8950f54536d40093b0a
Hyojun Cho <lionjerher84@gmail.com>
2024-01-15 01:37:37 +0900

    feat: reorderable_grid 적용

--- lib/ui/main/main_view_model.dart
+++ lib/ui/main/main_view_model.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_application/data/model/note_item.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:provider/provider.dart';
-
-import '../../data/model/note_item.dart';
 
 class MainViewModel extends ChangeNotifier {
-  late Box<NoteItem> items;
+  late Box<NoteItem> noteDB;
+  List<NoteItem> noteList = [];
 
   MainViewModel() {
     init();
   }
 
   Future<void> init() async {
-    items  = await Hive.openBox<NoteItem>('note.db');
+    try {
+      noteList = [];
+      noteDB = await Hive.openBox<NoteItem>('note.db');
+      noteList.addAll([...noteDB.values.whereType<NoteItem>()]);
+      notifyListeners();
+    } catch (e) {
+      throw Exception(e);
+    }
   }
+
+  /// 노트 아이템 삭제
+  Future<bool?> delete({required int index}) async {
+    try {
+      NoteItem getItem = findItem(index: index);
+      if (getItem != null) {
+        await noteDB.deleteAt(index);
+        init();
+        return true;
+      }
+    } catch (e) {
+      // print(e);
+      init();
+      return false;
+    }
+  }
+
+  /// 노트 아이템 검색
+  NoteItem findItem({required int index}) => noteList[index];
 }
--- lib/ui/main/widget/note_list_widget.dart
+++ lib/ui/main/widget/note_list_widget.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_note_application/main.dart';
 import 'package:flutter_note_application/ui/main/main_view_model.dart';
 import 'package:flutter_note_application/utils/commons.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
+import 'package:flutter_reorderable_grid_view/entities/order_update_entity.dart';
+import 'package:flutter_reorderable_grid_view/widgets/widgets.dart';
 
 class NoteListWidget extends StatefulWidget {
   const NoteListWidget({super.key});
@@ -14,97 +15,156 @@ class NoteListWidget extends StatefulWidget {
 }
 
 class _NoteListWidgetState extends State<NoteListWidget> {
-
   @override
   Widget build(BuildContext context) {
+    final _scrollController = ScrollController();
+    final _gridViewKey = GlobalKey();
+
     MainViewModel mainViewModel = context.watch<MainViewModel>();
-    return Positioned(
-      top: 10,
-      bottom: 10,
-      left: 10,
-      right: 10,
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-        child: ListView.builder(
-          itemCount: noteItems.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-              child: InkWell(
-                onTap: () {
-                  context.push('/editScreen/${noteItems.getAt(index)?.id}');
-                },
-                child: Card(
-                  color: Commons.convertIndexToColor(
-                      index: noteItems.getAt(index)!.color.toString()),
-                  child: Container(
-                    color: Colors.transparent, // 투명한 배경색으로 설정
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Stack(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                noteItems.getAt(index)?.title.toString() ?? '',
+
+    final List<Widget> generatedChildren =
+    mainViewModel.noteList.asMap().entries.map((entry) {
+      final index = entry.key;
+      final item = entry.value;
+
+      /// 노트 리스트 - 그리드 UI 설정
+      return Padding(
+        key: Key(item.id.toString()),
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+        child: InkWell(
+          onTap: () {
+            print('${item.id.toString()}');
+            context.push('/editScreen/${item.id.toString()}');
+          },
+          child: Card(
+            color: (Commons.convertIndexToColor(index: item.color.toString())
+            as Color)
+                .withOpacity(0.8),
+            child: Container(
+              color: Colors.transparent, // 투명한 배경색으로 설정
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Stack(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 7,
+                              child: Text(
+                                item.title.toString() ?? '',
                                 style: const TextStyle(
-                                  fontSize: 16.0,
+                                  fontSize: 24.0,
                                   fontWeight: FontWeight.bold,
                                   color: Colors.black,
                                 ),
                                 overflow: TextOverflow.ellipsis,
                                 maxLines: 1,
                               ),
-                              const SizedBox(height: 20),
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),
-                                child: Text(
-                                  mainViewModel.items.getAt(index)?.content.toString() ??
-                                      '',
-                                  style: const TextStyle(
-                                    fontSize: 16.0,
-                                    color: Colors.black,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
+                            ),
+                            const Spacer(),
+                            Expanded(
+                              flex: 3,
+                              child: Text(
+                                Commons.convertTime(
+                                    timeStamp: item.timeStamp) ??
+                                    '',
+                                style: const TextStyle(
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
                                 ),
-                              ),
-                            ],
-                          ),
-                          Positioned(
-                            top: 40,
-                            bottom: 0,
-                            right: -10,
-                            child:
-                            IconButton(
-                              onPressed: () async {
-                                await mainViewModel.items.deleteAt(index);
-                                setState(() {
-                                  Commons.showSnackBar(
-                                      context: context, message: '삭제 되었습니다.');
-                                });
-                              },
-                              icon: const Icon(
-                                Icons.delete,
-                                color: Colors.black,
+                                textAlign: TextAlign.right,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
                               ),
                             ),
-                          ),
-                        ],
-                      ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  flex: 8,
+                                  child: Text(
+                                    mainViewModel.noteList[index].content.toString() ?? '',
+                                    style: const TextStyle(
+                                      fontSize: 20.0,
+                                      color: Colors.black,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                  ),
+                                ),
+                                const Spacer(),
+                                Expanded(
+                                  flex: 2,
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                    child: IconButton(
+                                      onPressed: () async {
+
+                                        bool? isConfirm = await Commons.onAlertDialog(context: context, title: '삭제', content: '삭제 하시겠습니까?');
+
+                                        if(isConfirm != null && isConfirm){
+                                          bool? result = await mainViewModel.delete(index: index );
+
+                                          result != null
+                                              ? await Commons.showSnackBar(context: context, message: '삭제 되었습니다.')
+                                              : await Commons.showSnackBar(context: context, message: '삭제 실패.');
+
+                                        }
+                                      },
+                                      icon: const Icon(
+                                        Icons.delete,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
                     ),
-                  ),
+                  ],
                 ),
               ),
-            );
-          },
+            ),
+          ),
         ),
-      ),
+      );
+    }).toList();
+
+    /// Reorderable 설정
+    return ReorderableBuilder(
+      automaticScrollExtent: 10,
+      scrollController: _scrollController,
+      onReorder: (List<OrderUpdateEntity> orderUpdateEntities) {
+        for (final orderUpdateEntity in orderUpdateEntities) {
+          final item =
+          mainViewModel.noteList.removeAt(orderUpdateEntity.oldIndex);
+          mainViewModel.noteList.insert(orderUpdateEntity.newIndex, item);
+        }
+      },
+      builder: (children) {
+        return GridView(
+          key: _gridViewKey,
+          controller: _scrollController,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 1, // 가로 개수
+            childAspectRatio: (1 / .4), // 그리드 1칸 사이즈
+            mainAxisSpacing: 0, // 간격
+            crossAxisSpacing: 0,
+          ),
+          children: children,
+        );
+      },
+      children: generatedChildren,
     );
   }
 }
-
-
-
-
